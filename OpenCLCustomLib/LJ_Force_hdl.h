0a183482a1267341c63ed489fbbfce8ed4b51cc6
Chen Yang <ethanyang4623@gmail.com>
2018-07-26 16:11:48 -0400

    add host and kernel code, add compilation script

---
+++ OpenCLCustomLib/LJ_Force_hdl.h
@@ -0,0 +1,1 @@
+float4 LJ_Force_hdl(float ref_x, float ref_y, float ref_z, float neighbor_x, float neighbor_y, float neighbor_z);
---
+++ OpenCLCustomLib/gen_library.sh
@@ -0,0 +1,2 @@
+aoc -c LJ_Force_hdl.xml -o LJ_Force_hld.aoco
+aocl library create -o LJ_Force_hdl.aoclib LJ_Force_hdl.aoco
